c281df4e0607e55cb401ceacb2b3f1c7b12fd0f9
Daw99y <k@ecksdee.org>
2025-12-13 17:07:51 +0800

    Cowvert - 3.35 - Fixed grey container.

--- app/learn/components/SectionCTA.tsx
+++ app/learn/components/SectionCTA.tsx
@@ -1,5 +1,5 @@
 export default function SectionCTA() {
   // Spacer section to push content above the grass at the bottom
   // Increased height to accommodate the footer overlay
-  return <div className="h-[30vh] min-h-[250px] bg-transparent" />;
+  return <div className="h-[30vh] min-h-[250px] bg-white" />;
 }
